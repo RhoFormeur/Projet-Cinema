256b669af136786b03e80208709d1ed4a170eb9b
RhoFormeur <ledusmaxime@gmail.com>
2023-01-25 17:03:39 +0100

    remove .env file from git

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require("dotenv").config({path: "./vars/.env"});
 const mysql = require("mysql")
 const express = require("express")
 const { engine } = require("express-handlebars")
